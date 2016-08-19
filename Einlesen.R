29f97271b77465b4a1de3cfb632e3e53c7204d55
ycphs <philipp@schauberger.co.at>
2016-08-19 18:15:17 +0200

    Ausgangsskript

---
+++ Einlesen.R
@@ -0,0 +1,31 @@
+## Wenn nicht installiert dann installieren
+library(data.table)
+library(openxlsx)
+
+# install.packages('data.table',dependencies = T)
+# install.packages('openxlsx',dependencies = T)
+
+## Wenn nicht installiert dann installieren
+
+# ?assign
+
+x <- fread("MODELL1.DAT", header = T)
+
+
+for (i in seq_along(x$Wert))
+{
+      assign(x$Variable[i], x$Wert[i])
+}
+
+
+
+
+x <- read.xlsx("parameter.xlsx")
+setDT(x)
+
+
+
+for (i in seq_along(x$Wert))
+{
+      assign(x$Variable[i], x$Wert[i])
+}
